4b847e1918d04884ab1ce2d86a8c4d69e5b40d0b
Shakhar Dasgupta <shakhardasgupta@gmail.com>
2016-02-28 13:48:02 -0500

    Changed booleanValue() to intValue()
    Fixed issue with call of booleanValue, as the signal is given as an int and not a boolean.

--- src/muselightswitch/MuseOscServer.java
+++ src/muselightswitch/MuseOscServer.java
@@ -60,7 +60,7 @@ public class MuseOscServer {
      */
     public void oscEvent(OscMessage msg) {
         if (msg.checkAddress("/muse/elements/jaw_clench")) {
-            if (msg.get(0).booleanValue()) {
+            if (msg.get(0).intValue() == 1) {
                 if (this.done) {
                     this.lightSwitch.toggleState();
                     this.done = false;
